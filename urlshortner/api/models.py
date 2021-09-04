1e3646496e39d7ad6eeada102c3b520ef432e8d7
mahinm20 <mahinmalhotra20@gmail.com>
2021-09-05 02:06:54 +0530

    added models

--- urlshortner/api/models.py
+++ urlshortner/api/models.py
@@ -1,3 +1,27 @@
+from hashlib import new
 from django.db import models
+from random import choice, choices
+from string import ascii_letters
+from django.conf import settings
 
 # Create your models here.
+class Link(models.Model):
+    original_url = models.URLField()
+    short_url=models.URLField(blank=True,null=True)
+
+    def shortenerURL(self):
+        STRING_PARAMTER = ascii_letters + "1234567890"
+        while True:
+            str1="".join(choices(STRING_PARAMTER,k=8))
+
+            new_url = settings.HOST_URL +"/"+str1
+            if not Link.objects.filter(short_url=new_url).exists():
+                break
+
+        return new_url
+
+    def save(self,*args, **kwargs):
+        if not self.short_url:
+            new_url=self.shortenerURL()
+            self.short_url=new_url
+        return super().save(*args, **kwargs)            
