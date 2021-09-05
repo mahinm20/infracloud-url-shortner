68c7fcb64e77752c7cc5abae710caf7bde5b1a47
mahinm20 <mahinmalhotra20@gmail.com>
2021-09-05 19:01:03 +0530

    working commit

--- urlshortner/api/urls.py
+++ urlshortner/api/urls.py
@@ -1,6 +1,7 @@
 from re import I
 from django.urls import path
 from .views import ShortnerCreateAPIview,ShortnerListAPIView
+from api.views import Redirect
 
 appname="api"
 
@@ -9,4 +10,5 @@ appname="api"
 urlpatterns = [
     path('',ShortnerListAPIView.as_view(),name='links'),
     path('create/',ShortnerCreateAPIview.as_view(),name='create_link'),
+    
 ]
