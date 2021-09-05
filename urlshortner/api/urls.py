b9506a8243c0da7d044aa8ed4d66c59cb86b51f2
mahinm20 <mahinmalhotra20@gmail.com>
2021-09-05 18:15:35 +0530

    added url paths

--- urlshortner/api/urls.py
+++ urlshortner/api/urls.py
@@ -1,5 +1,12 @@
+from re import I
+from django.urls import path
+from .views import ShortnerCreateAPIview,ShortnerListAPIView
+
 appname="api"
 
+
+
 urlpatterns = [
-    
+    path('',ShortnerListAPIView.as_view(),name='links'),
+    path('create/',ShortnerCreateAPIview.as_view(),name='create_link'),
 ]
--- urlshortner/api/views.py
+++ urlshortner/api/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django import views
+from django.shortcuts import render, redirect
 from rest_framework.generics import ListAPIView,CreateAPIView
 from .models import Link
 from .serializer import LinkSerializer
 from django.views import View
+from django.conf import settings
 # Create your views here.
 
 class ShortnerListAPIView(ListAPIView):
@@ -13,4 +15,8 @@ class ShortnerListAPIView(ListAPIView):
 class ShortnerCreateAPIview(CreateAPIView):
     serializer_class = LinkSerializer
 
-class Redirect()    
+class Redirect(View):
+    def get(self,request,shortener_link,*args, **kwargs):
+        shortener_link=settings.HOST_URL+'/'+self.kwargs['shortener_link']
+        redirect_link = Link.objects.filter(short_url=shortener_link).first().original_url
+        return redirect(redirect_link)
